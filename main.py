9feb098ae05553533ae37afd6bf4fc3e938d2ffd
BIZONUSERYAKAMO <2860483411@qq.com>
2025-01-08 12:08:41 +0800

    注释+创建文件

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 #!/bin/python3
 
 import sys
+import os
 
 def ed_mode(filename):
     currentLns = 0
     while True:
+        # Create file if not exists
+        if os.path.exists(filename):
+            pass
+        else:
+            with open(filename, 'w', encoding='utf-8') as f:
+                f.write('')
+                f.close()
+        # Read file
         with open(filename, 'r', encoding='utf-8') as f:
             f.seek(0)  
             file = f.read()
@@ -15,7 +24,9 @@ def ed_mode(filename):
             for i in range(len(fileLine)):
                 print(f"{i+1} | {fileLine[i]}")
             print("==================")
+        # Input Module
         while True:
+            # Read shell input
             shinput = input(f'[Ln {currentLns+1}] > ')
             if shinput in ('.nextline', '.nl'):
                 currentLns += 1
@@ -26,10 +37,12 @@ def ed_mode(filename):
                     f.close()
                     break
                 break
+            # Prev line
             elif shinput in ('.prevline', '.pl'):
                 currentLns -= 1
                 f.close()
                 break
+            # Clean all
             elif shinput in ('.cleanall'):
                 choice = input('Really? [Y/N] ')
                 if choice.lower() == 'y':
@@ -40,6 +53,7 @@ def ed_mode(filename):
                         f.write('\n'.join(fileLine))
                         f.close()
                         break
+            # Replace
             elif shinput in ('.replace', '.r'):
                 target = input('Target? > ')
                 replace = input('Replace? > ')
@@ -51,6 +65,7 @@ def ed_mode(filename):
                     f.write('\n'.join(fileLine))
                     f.close()
                     break
+            # Writing
             else:
                 fileLine[currentLns] = shinput
                 f.close()
